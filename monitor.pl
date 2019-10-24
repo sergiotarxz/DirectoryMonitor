de5f8aa3e1f855c4a52d729ff5d5bd5427e6e7d3
Matteo Cantoni <matteo.cantoni@gmail.com>
2019-10-24 19:47:57 +0200

    Add email support and some cosmetic tweaks

--- monitor.pl
+++ monitor.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 #
 # Copyright (C) 2019  Sergio Iglesias
 # 
@@ -15,52 +17,104 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+$|=1;
+
 use strict;
 use warnings;
 use feature qw(say);
-use POSIX qw(strftime);
-use Digest::file qw(digest_file);
 
-my $dir = $ARGV[0] or die 'No file received';
--e $dir or die 'No such file';
--d $dir or die 'Not a dir';
-my %files;
-sub index_dir;
-sub monitor {
-    my $date = localtime;
-    my $a = 0;
-    my %old_hash = %files;
-    my %changes;
-    %files = ();
-    index_dir $_[0];
-    if (%old_hash) {
-        for (keys %files) {
-            $old_hash{$_} or do {$changes{$_} = 'Created'; next};
-            $old_hash{$_} eq $files{$_} or $changes{$_} = 'Modified';
-        }
-        for (keys %old_hash) {
-            $files{$_} or $changes{$_} = 'Removed';
-        }
-    }
-    %changes and say strftime "%d/%m/%Y", localtime;
-    for (keys %changes) {
-        say "$changes{$_} $_";
-    }
+use Digest::file qw(digest_file);
+use Email::MIME;
+use Email::Sender::Simple qw(sendmail);
+use Email::Sender::Transport::SMTP::TLS;
+use POSIX qw(strftime);
+
+############################################ default settings ############################################
+my $email_from     = '';
+my $email_to       = '';
+my $email_subject  = 'Directory Monitor';
+my $smtp_host      = 'smtp.gmail.com';
+my $smtp_port      = 587;
+my $smtp_username  = '',
+my $smtp_password  = '';
+##########################################################################################################
+
+$SIG{INT} = sub {
+  die("Exit...\n");
+};
+
+my $dir = $ARGV[0] or die "No file received\nUsage: $0 <file/dir>\n";
+-e $dir or die "No such file\n";
+-d $dir or die "Not a dir\n";
+
+my %db_files;
+
+while (1) {
+  sleep 0.5;
+  Monitor($dir);
 }
-sub index_dir {
-    my $path = "$_[0]";
-    opendir(my $dh, $path);
+
+sub Monitor {
+  my $path = shift;
+  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time); $year = $year+1900; $mon = $mon+1;
+  my $datetime = sprintf "%04d-%02d-%02d %02d:%02d:%02d",($year,$mon,$mday,$hour,$min,$sec);
+  my %old_hash = %db_files;
+  my %changes;
+  %db_files = ();
+  Index_dir($path);
+  if (%old_hash) {
+    for (keys %db_files) {
+      $old_hash{$_} or do {$changes{$_} = 'Created:'; next};
+      $old_hash{$_} eq $db_files{$_} or $changes{$_} = 'Modified:';
+    }
+    for (keys %old_hash) {
+      $db_files{$_} or $changes{$_} = 'Removed:';
+    }
+  }
+  %changes and say $datetime;
+  for (keys %changes) {
+    say "$changes{$_} $_";
+    Send_Mail("$datetime - $changes{$_} $_");
+  }
+}
+
+sub Index_dir {
+  my $path = shift;
+  opendir(my $dh, $path);
     my @contents = readdir $dh;
     for (@contents) {
-        my $file = "$path/$_";
-        /^\.{1,2}$/ and next;
-        $file =~ s/\/+/\//g;
-        -f $file and $files{$file} = digest_file($file, 'MD5');
-        -d $file and index_dir $file;
+      my $file = "$path/$_";
+      /^\.{1,2}$/ and next;
+      $file =~ s/\/+/\//g;
+      -f $file and $db_files{$file} = digest_file($file, 'MD5');
+      -d $file and Index_dir($file);
     }
-    closedir $dh;
+  closedir $dh;
 }
-while (1) {
-    sleep 0.5;
-    monitor $ARGV[0];
+
+sub Send_Mail {
+  my $body = shift;
+
+  my $email_object = Email::MIME->create(
+    header => [
+      From         => $email_from,
+      To           => $email_to,
+      Subject      => $email_subject,
+      content_type => 'multipart/mixed'
+    ],
+    attributes => {
+      content_type => "text/html",
+      charset      => "ISO-8859-1",
+    },
+    body => "$body\n",
+  );
+  
+  my $transport = Email::Sender::Transport::SMTP::TLS->new(
+    host     => $smtp_host,
+    port     => $smtp_port,
+    username => $smtp_username,
+    password => $smtp_password
+  );
+  
+  sendmail( $email_object, {transport => $transport} );
 }
